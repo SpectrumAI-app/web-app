579b2a086c5e43b60231ab5302a3c4f0e8836231
oleh <oleh.a.falko@gmail.com>
2023-10-05 19:57:14 +0200

    implement admin login and routes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <HelloWorld />
+  <router-view></router-view>
 </template>
 
-<style scoped>
-
+<style>
+  @import "App.scss";
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+        scss: {
+            additionalData: `@use "./src/utils/styles/main.scss" as *;`,
+        }
+    }
+  },
   plugins: [vue()],
   base: '/web-app/'
 })
