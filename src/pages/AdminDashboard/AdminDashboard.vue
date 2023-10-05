e6ff830380a76df8154afc22e3bb3c3ba4a55461
oleh <oleh.a.falko@gmail.com>
2023-10-05 19:57:33 +0200

    implement admin dashboard

---
+++ src/pages/AdminDashboard/AdminDashboard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="admin-dashboard">
+    <!--  Add variation for navbar component  -->
+    <div class="admin-dashboard__navbar">
+      <img src="https://placehold.co/150x150?text=Logo" />
+      <Link v-for="link in navbarLinks" :to="link.to">
+        {{ link.text }}
+      </Link>
+      <Link class="admin-dashboard__logout" to="/">
+        <template #appendIcon>
+          <Icon :icon="['fas', 'sign-out']" />
+        </template>
+        Log out
+      </Link>
+    </div>
+    <div class="admin-dashboard__content">
+      <h2>Welcome to Admin Dashboard!</h2>
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Tile from "../../components/Tile/Tile.vue";
+import Button from "../../components/Button/Button.vue";
+import Link from "../../components/Link/Link.vue";
+export default {
+  name: "AdminDashboard",
+  components: {Link, Button, Tile},
+  data() {
+    return {
+      navbarLinks: [
+        {
+          text: "Dashboard",
+          to: "/admin-dashboard"
+        },
+        {
+          text: "Blog edit",
+          to: "/blog"
+        },
+        {
+          text: "Analytics",
+          to: "/analytics"
+        },
+        {
+          text: "Spectrum Webpage",
+          to: "/home"
+        }
+      ],
+    }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import "AdminDashboard.scss";
+</style>
---
+++ src/pages/AdminDashboard/AdminTiles/AdminTiles.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="admin-dashboard__tiles">
+    <div class="admin-dashboard__tile" v-for="tile in tiles">
+      <Tile>
+        <template #header>
+          <img :src="tile.header" />
+        </template>
+        <template #content>
+          <p>{{tile.content}}</p>
+        </template>
+        <template #footer>
+          <Button @click="open(tile.to)">{{ tile.footer }}</Button>
+        </template>
+      </Tile>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../../../components/Button/Button.vue";
+import Tile from "../../../components/Tile/Tile.vue";
+
+export default {
+  name: "AdminTiles",
+  components: {Button, Tile},
+  data() {
+    return {
+      tiles: [
+        {
+          header: "https://placehold.co/350x350?text=SEX",
+          content: "Blogs",
+          footer: "Go to Blogs",
+          to: "/blog"
+        },
+        {
+          header: "https://placehold.co/350x350?text=SEX",
+          content: "Analytics",
+          footer: "Go to Analytics",
+          to: "/blog"
+        },
+        {
+          header: "https://placehold.co/350x350?text=SEX",
+          content: "Add/Edit Blog",
+          footer: "Add/Edit Blog",
+          to: "/blog"
+        },
+        {
+          header: "https://placehold.co/350x350?text=SEX",
+          content: "Add/Edit Blog",
+          footer: "Add/Edit Blog",
+          to: "/blog"
+        }
+      ]
+    }
+  },
+  methods: {
+    open(to: string) {
+      this.$router.push(to);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  @import "AdminTiles.scss";
+</style>
